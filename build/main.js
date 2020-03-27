81d42538649a0ec5157683eea6097e287b069cd8
Simon Lebedev <sem.schakschin2010@yandex.ru>
2020-03-27 13:06:11 +0300

    try coonect pqsl...

--- build/main.js
+++ build/main.js
@@ -95,18 +95,28 @@ module.exports =
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+// console.log(process.env.POSTGRESS_HOST, process.env.POSTGRESS_USER, process.env.POSTGRESS_PASSWORD, process.env.POSTGRESS_DB, process.env.POSTGRESS_CHARSET);
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRESS_HOST,
-      user: process.env.POSTGRESS_USER,
-      password: process.env.POSTGRESS_PASSWORD,
-      database: process.env.POSTGRESS_DB
+      host: '127.0.0.1',
+      port: '5432',
+      //5432
+      user: 'postgres',
+      password: '1',
+      database: 'chat' // connectTimeout: 90000
+
     },
     pool: {
       min: 0,
-      max: 7
+      max: 7 // createTimeoutMillis: 3000,
+      // acquireTimeoutMillis: 30000,
+      // idleTimeoutMillis: 30000,
+      // reapIntervalMillis: 1000,
+      // createRetryIntervalMillis: 100,
+      // propagateCreateError: false
+
     }
   }
 };
